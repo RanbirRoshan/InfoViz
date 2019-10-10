69c960ca4492dd27734e0ccd83ceea8b4b4188c3
Varun Reddy Regalla <varunreddy1061@gmail.com>
2019-10-10 01:50:44 -0400

    added supplement page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var logger = require("morgan")
 var bodyParser = require("body-parser")
 var neo4j = require('neo4j-driver').v1;
 var app = express();
+var cors = require('cors');
 var driver = neo4j.driver("bolt://192.168.0.50:7687");
 
 
@@ -13,23 +14,45 @@ app.set("views", path.join(__dirname, "/src/views"))
 app.set("view engine", "ejs")
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
-
+app.use(cors())
 app.get("/", function (req, res) {
-    dbUtil.pingServer(req, res,driver)
+    dbUtil.pingServer(req, res, driver)
 })
 
 app.get("/KeyWords", function (req, res) {
-    dbUtil.KeyWordSearch(res, 1, "A", 25, driver)
+    console.log(req.body.type);
+    dbUtil.KeyWordSearch(res, req.query.type, req.query.name, 25, driver)
 })
 
 app.get("/a", function (req, res) {
 
-    dbUtil.MedDSInteraction(["ethanols","Retinoic Acids"], ["Met-Rx - Pure Protein Shake Vanilla Cream"], driver);
+    dbUtil.MedDSInteraction(["ethanols", "Retinoic Acids"], ["Met-Rx - Pure Protein Shake Vanilla Cream"], driver);
     dbUtil.MedDSInteraction(["ethanols"], ["Met-Rx - Pure Protein Shake Vanilla Cream"], driver);
     res.render("supVsmed")
 })
+
+var supp = [];
+var drug = [];
+app.get("/app", function (req, res) {
+
+    res.render("supplements", { supp: supp, drug: drug });
+
+})
+
+app.post("/app", function (req, res) {
+    // res.render("/a", { type: req.body.type, name: req.body.name });
+    // console.log(req.body.name);
+    if (req.body.type == "Supplement") {
+        supp.push({ type: req.body.type, name: req.body.name });
+    } else {
+        drug.push({ type: req.body.type, name: req.body.name });
+    }
+    // console.log(names);
+    res.render("supplements", { supp: supp, drug: drug });
+})
+
 
 
 app.listen(8080);
--- src/db_utils.js
+++ src/db_utils.js
@@ -1,103 +1,103 @@
 
-exports.KeyWordSearch = function (res, type, SearchPrifix, WordCount, driver){
-    var session  = driver.session();
+exports.KeyWordSearch = function (res, type, SearchPrifix, WordCount, driver) {
+    var session = driver.session();
     console.log(type)
     console.log(SearchPrifix)
     console.log(WordCount)
     // 1 stands for Suppliment
-    if (type == 1){
-        var ret = session.run('MATCH (n:DSP) where n.name =~ \"(?i)'+SearchPrifix+'.*\" RETURN n LIMIT $OutLimit', {OutLimit: WordCount})
-    } else if (type == 2){
-        var ret = session.run('MATCH (n:SPD) where n.name =~ \"(?i)'+SearchPrifix+'.*\" RETURN n LIMIT $OutLimit', {OutLimit: WordCount})
+    if (type == 1) {
+        var ret = session.run('MATCH (n:DSP) where n.name =~ \"(?i)' + SearchPrifix + '.*\" RETURN n LIMIT $OutLimit', { OutLimit: WordCount })
+    } else if (type == 2) {
+        var ret = session.run('MATCH (n:SPD) where n.name =~ \"(?i)' + SearchPrifix + '.*\" RETURN n LIMIT $OutLimit', { OutLimit: WordCount })
     } else {
-        var ret = session.run('MATCH (n) where n.name =~ \"(?i)'+SearchPrifix+'.*\" RETURN n LIMIT $OutLimit', {OutLimit: WordCount})
+        var ret = session.run('MATCH (n) where n.name =~ \"(?i)' + SearchPrifix + '.*\" RETURN n LIMIT $OutLimit', { OutLimit: WordCount })
     }
-    ret.then(function(result) {
+    ret.then(function (result) {
         var list = []
-        result.records.forEach(function(item)
-        {
-           list.push(item._fields[0].properties.name)
+        result.records.forEach(function (item) {
+            list.push(item._fields[0].properties.name)
         })
         res.send(list)
         session.close();
+    }
+    )
+        .catch(function (err) {
+            console.log(err.toString())
         }
-    )
-    .catch (function(err){
-        console.log(err.toString())}
-    )
+        )
 }
 
 
 //var session = driver.session();
-exports.MedDSInteraction = function (medList, DSList, driver){
+exports.MedDSInteraction = function (medList, DSList, driver) {
     if (medList.length == 0 || DSList.length == 0)
         return []
 
     if (medList.length != 1 && DSList.length != 1)
-        return  []
+        return []
 
-    var session  = driver.session();
+    var session = driver.session();
 
-    if (medList.length == 1){
+    if (medList.length == 1) {
         var ret = session.run('MATCH (a:DSP)-[:has_ingredient]->(b)-[:interacts_with]->(c:SPD{name:$SPDName}) ' +
-            'where a.name in $SupList  RETURN a', {SupList: DSList, SPDName: medList[0]})
+            'where a.name in $SupList  RETURN a', { SupList: DSList, SPDName: medList[0] })
     } else {
         var ret = session.run('MATCH (a:DSP{name:$SupName})-[:has_ingredient]->(b)' +
-            '-[:interacts_with]->(c) where c.name in $MedList  RETURN c', {MedList: medList, SupName: DSList[0]})
+            '-[:interacts_with]->(c) where c.name in $MedList  RETURN c', { MedList: medList, SupName: DSList[0] })
     }
-    ret.then(function(result) {
-            //  result.records.forEach(function (data) {
-            //      list.push(data._fields[0].properties.name)
-            //      //console.log(data._fields[0].properties.name);
-            // })
-            // list.forEach(function(item)
-            // {
-            //    // res.write(item)
-            // })
-            // res.send(list)
-            console.log(result)
-            res.send()
-            session.close();
-        }
+    ret.then(function (result) {
+        //  result.records.forEach(function (data) {
+        //      list.push(data._fields[0].properties.name)
+        //      //console.log(data._fields[0].properties.name);
+        // })
+        // list.forEach(function(item)
+        // {
+        //    // res.write(item)
+        // })
+        // res.send(list)
+        console.log(result)
+        res.send()
+        session.close();
+    }
     )
-        .catch (function(err){
-            console.log(err.toString())}
+        .catch(function (err) {
+            console.log(err.toString())
+        }
         )
 }
 
 exports.pingServer = function (req, res, driver) {
 
     var list = []
-    var session  = driver.session();
+    var session = driver.session();
 
     //var ret = session.run('MATCH (p:SDSI)<-[:has_ingredient]-(n) where p.name="Lysine" or p.name="Iron" RETURN n')
     var ret = session.run('MATCH p=()-[r:interacts_with]->() RETURN p LIMIT 10')
-    console.log("hi")
+    // console.log("hi")
     //wait till ret has returned
-    ret.then(function(result) {
-            //  result.records.forEach(function (data) {
-            //      list.push(data._fields[0].properties.name)
-            //      //console.log(data._fields[0].properties.name);
-            // })
-            // list.forEach(function(item)
-            // {
-            //    // res.write(item)
-            // })
-            // res.send(list)
-        console.log(result.records[0]._fields[0].segments[0].relationship)
-            res.send()
-            session.close();
+    ret.then(function (result) {
+        //  result.records.forEach(function (data) {
+        //      list.push(data._fields[0].properties.name)
+        //      //console.log(data._fields[0].properties.name);
+        // })
+        // list.forEach(function(item)
+        // {
+        //    // res.write(item)
+        // })
+        // res.send(list)
+        res.send()
+        session.close();
+    }
+    )
+        .catch(function (err) {
+            console.log(err.toString())
         }
         )
-    .catch (function(err){
-        console.log(err.toString())}
-        )
 
-    console.log("end")
-    list.forEach(function(item)
-    {
-        console.log(item)
-    })
+    // console.log("end")
+    // list.forEach(function (item) {
+    //     console.log(item)
+    // })
 
     return "Server Ping Called Jay";
 }
