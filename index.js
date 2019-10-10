66cfd679da810623ad77e87961b8e1ab299c497c
Ranbir Roshan <ranbirroshan@live.in>
2019-10-09 20:57:55 -0400

    Signed-off-by: Ranbir Roshan <ranbirroshan@live.in>

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var http = require('http');
-var serverUtil = require('./src/db_utils')
+var dbUtil = require('./src/db_utils')
+var express = require("express")
+var path = require("path")
+var logger = require("morgan")
+var bodyParser = require("body-parser")
+var neo4j = require('neo4j-driver').v1;
+var app = express();
+var driver = neo4j.driver("bolt://127.0.0.1:7687");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("Pining Server response: " + serverUtil.pingServer())
-    res.end();
-}).listen(8080);
+
+app.set("views", path.join(__dirname, "/src/views"))
+app.set("view engine", "ejs")
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", function (req, res) {
+    dbUtil.pingServer(req, res,driver)
+})
+
+app.get("/a", function (req, res) {
+    res.render("supVsmed")
+})
+
+app.listen(8080);
+console.log("server started")
+module.exports = app
--- src/db_utils.js
+++ src/db_utils.js
@@ -1,22 +1,38 @@
 
 //var session = driver.session();
-exports.pingServer = function () {
-    var neo4j = require('neo4j-driver').v1;
-    var driver = neo4j.driver("bolt://127.0.0.1:7687");
+exports.pingServer = function (req, res, driver) {
+
+    var list = []
     var session  = driver.session();
 
-    var ret = session.run('MATCH p=()-[r:has_ingredient]->() RETURN p LIMIT 25')
-        .then(function(result) {
-                result.records.forEach(function (data) {
-                    console.log(data);
-                })
-                session.close();
-                driver.close()
-            }
-            )
-        .catch (function(err){
-            console.log(err.toString())}
-            )
+    //var ret = session.run('MATCH (p:SDSI)<-[:has_ingredient]-(n) where p.name="Lysine" or p.name="Iron" RETURN n')
+    var ret = session.run('MATCH p=()-[r:interacts_with]->() RETURN p LIMIT 10')
+    console.log("hi")
+    //wait till ret has returned
+    ret.then(function(result) {
+            //  result.records.forEach(function (data) {
+            //      list.push(data._fields[0].properties.name)
+            //      //console.log(data._fields[0].properties.name);
+            // })
+            // list.forEach(function(item)
+            // {
+            //    // res.write(item)
+            // })
+            // res.send(list)
+        console.log(result.records[0]._fields[0].segments[0].relationship)
+            res.send()
+            session.close();
+        }
+        )
+    .catch (function(err){
+        console.log(err.toString())}
+        )
+
+    console.log("end")
+    list.forEach(function(item)
+    {
+        console.log(item)
+    })
 
     return "Server Ping Called";
 }
