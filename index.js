385d692a549ccd3af0ee6d8bcf97f4c00ab9b74e
Ranbir Roshan <ranbirroshan@live.in>
2019-10-10 13:02:09 -0400

    Visual look change for side list
    Signed-off-by: Ranbir Roshan <ranbirroshan@live.in>

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ var neo4j = require('neo4j-driver').v1;
 var app = express();
 var cors = require('cors');
 
-var driver = neo4j.driver("bolt://168.168.43.5:7687");
-
+var driver = neo4j.driver("bolt://localhost:7687");
 
 app.set("views", path.join(__dirname, "/src/views"))
 app.set("view engine", "ejs")
